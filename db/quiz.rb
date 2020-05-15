c01e00f2476b71363d53dd6d780a9897905adac5
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-05-15 18:14:45 +0100

    cocktail fix

---
+++ db/quiz.rb
@@ -0,0 +1,28 @@
+# TODO: Below are three questions for you to answer. Read each of the questions
+# and make sure each of the methods return the correct answer.
+# :warning: Try and answer them before running rake.
+def restaurants_resources_routes
+ # TODO: Return an Array with the 7 routes that resources :restaurants would generate for you.
+ routes = []
+ routes << “get ‘/restaurants’, to: ‘restaurants#index’”
+ routes << “get ‘/restaurants/new’, to: ‘restaurants#new’”
+ routes << “get ‘/restaurants/:id’, to: ‘restaurants#show’”
+ routes << “post ‘/restaurants’, to: ‘restaurants#create’”
+ routes << “get ‘/restaurants/:id/edit’, to: ‘restaurants#edit’”
+ routes << “patch ‘/restaurants/:id’, to: ‘restaurants#update’”
+ routes << “delete ‘/restaurants/:id’, to: ‘restaurants#destroy’”
+ # Add more routes here
+ return routes
+end
+def nested_routes_for_one_to_many?
+ return false
+ # TODO: Return a true or false to answer this question:
+ # If you have a one to many relationship between you models like Restaurant and Review (belongs_to :restaurant),
+ # do you always have to nest all your routes for Review in Restaurant?
+end
+def validate_name
+ return ‘validates :name, presence: true’
+ # TODO: Return a string of the ActiveRecord validation need to make sure no record is
+ # created without a name. /!\ in Rails’ context, a validation **is not** a String!
+ # (it is only a String for the purpose of this exercise)
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,20 @@ require 'json'
 require 'open-uri'
 require 'rest-client'
 
-response = RestClient.get 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients = JSON.parse(response)
+# response = RestClient.get 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients = JSON.parse(response)
 
-Ingredient.destroy_all
+# Ingredient.destroy_all
 
-ingredients['drinks'].each do |ingredient|
-  Ingredient.create!(name: ingredient['strIngredient1'])
+# ingredients['drinks'].each do |ingredient|
+#   Ingredient.create!(name: ingredient['strIngredient1'])
+# end
+
+
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
 end
